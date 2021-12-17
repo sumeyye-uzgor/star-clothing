97956c6e64fa2e62d1a54d067c2fc1785843684e
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-12-17 18:13:19 +0300

    project font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Lemonada", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
